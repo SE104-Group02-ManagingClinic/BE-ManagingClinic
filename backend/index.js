085e4b6caa1c60a0261c9c24dff3a739109c5a19
TrThuyTien <148189997+TrThuyTien@users.noreply.github.com>
2025-11-04 13:10:39 +0700

    fix file app.js - line 4 (#2)

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
-})
+});
