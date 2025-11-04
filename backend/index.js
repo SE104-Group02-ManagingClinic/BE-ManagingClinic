42f54a09ae15f683271b81eb2e742a09979758c3
TrThuyTien <148189997+TrThuyTien@users.noreply.github.com>
2025-11-04 15:11:58 +0700

    config(database): create database connection (#3)

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 
 const app = require('./app');
+const mySqlPool = require('./config/database');
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+// Điều kiện mở kết nối server là kết nối thành công tới database trước
+mySqlPool.query("SELECT 1").then(() => {
+    // Kết nối tới database server thành công
+    console.log("MYSQL DB Connected.");
+    // Mở server 
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+    });
 });
+
