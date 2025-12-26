9fad96b8509142b09a86e46c56e83525f8d98be2
Trần Thị Thùy Tiên <148189997+TrThuyTien@users.noreply.github.com>
2025-12-26 23:03:27 +0700

    feat(permission): support adding many permissions (#44)

--- backend/services/permission_service.js
+++ backend/services/permission_service.js
@@ -1,51 +1,99 @@
 const db = require('../config/database');
 
 class PermissionService {
-    // Kiem tra phan quyen do da co trong db hay chua
-    static async existedPermission(MaNhom, MaChucNang) {
-        try {
-            const [rows] = await db.query(
-                "select * from PHANQUYEN where MaNhom = ? and MaChucNang = ?",
-                [MaNhom, MaChucNang]
-            );
-            if (rows.length > 0) return true;
-            return false;
-        }
-        catch (error) {
-
-        }
-    }
     // Them phan quyen moi
-    static async createPermission(MaNhom, MaChucNang) {
+    static async createPermissions(MaNhom, arrMaChucNang) {
         try {
-            const record = {
-                MaNhom,
-                MaChucNang
-            }
-            const result = await db.query(
-                "insert into PHANQUYEN set ?",
-                [record]
+            // Kiểm tra xem nhóm đã có phân quyền nào chưa
+            const [rows] = await db.query(
+                "select 1 from PHANQUYEN where MaNhom = ? limit 1",
+                [MaNhom]
             );
-            return record;
-        }
-        catch (error) {
-            console.log("Permission Service Error: ", error);
+
+            if (rows.length > 0) {
+                // Nhóm đã có phân quyền rồi -> không tạo mới
+                return false;
+            }
+
+            // Nếu chưa có -> thêm toàn bộ danh sách quyền
+            for (const MaChucNang of arrMaChucNang) {
+                await db.query(
+                    `insert into PHANQUYEN (MaNhom, MaChucNang) values (?, ?)`,
+                    [MaNhom, MaChucNang]
+                );
+            }
+
+            return true;
+        } catch (error) {
+            console.log("Permission Service createPermissions Error: ", error);
+            return null;
         }
     }
+
     // Xoa quyen
-    static async deletePermission(MaNhom, MaChucNang) {
+    static async deletePermissions(MaNhom, DSMaChucNang) {
         try {
-            const [rows] = await db.query(
-                "delete from PHANQUYEN where MaNhom = ? and MaChucNang = ?",
-                [MaNhom, MaChucNang]
-            );
-            if (rows.affectedRows === 0) return false;
+            const hasAnyPermission = await this.hasAnyPermission(MaNhom);
+            
+            if (!hasAnyPermission) {
+                return false;
+            }
+            await db.query( `
+                delete from PHANQUYEN 
+                where MaNhom = ? and MaChucNang in (?)`, 
+                [MaNhom, DSMaChucNang] 
+            ); 
             return true;
         }
         catch (error) {
             console.log("Permission Service deletePermission Error: ", error);
         }
     }
+
+    // Kiem tra nhom da duoc phan quyen chua
+    static async hasAnyPermission(MaNhom) { 
+        try { 
+            const [rows] = await db.query( `
+                select 1 from PHANQUYEN 
+                where MaNhom = ? limit 1`, 
+                [MaNhom]); 
+            return rows.length > 0; 
+        } 
+        catch (error) { 
+            console.log("Permission Service hasAnyPermission Error: ", error); 
+            return false; 
+        } 
+    }
+
+    // Cap nhat danh sach phan quyen
+    static async updatePermissions(MaNhom, DSMaChucNang) {
+        try {
+            const hasAnyPermission = await this.hasAnyPermission(MaNhom);
+            
+            if (!hasAnyPermission) {
+                await this.createPermission(MaNhom, DSMaChucNang);
+                return true;
+            }
+
+            const currPermissions = await  this.getFunctionsOfGroupUser(MaNhom);
+
+            const toAdd = DSMaChucNang.filter(x => !currPermissions.includes(x));
+            const toDelete = currPermissions.filter(x => !DSMaChucNang.includes(x));
+
+            if (toAdd.length > 0) {
+                await this.createPermissions(MaNhom, toAdd);
+            }
+            if (toDelete.length > 0) {
+                await this.deletePermissions(MaNhom, toDelete);
+            }
+            return true;
+        }
+        catch (error) {
+            console.log("Permission Service updatePermissions Error: ", error);
+            return false;
+        }
+    }
+
     // Lay danh sach cac chuc nang cua mot nhom
     static async getFunctionsOfGroupUser(MaNhom) {
         try {
