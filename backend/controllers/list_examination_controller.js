95b85c90ee46c2edc0f7d6d39f291bb8a793467a
trthuytien <tranthithuytien0615@gmail.com>
2025-12-28 08:16:49 +0700

    upload list examination

---
+++ backend/controllers/list_examination_controller.js
@@ -0,0 +1,74 @@
+const ListExamService = require('../services/list_examination_service');
+
+exports.addInList = async (req, res) => {
+    try {
+        const { NgayKham, MaBN } = req.body;
+
+        const data = { NgayKham, MaBN };
+
+        const result = await ListExamService.addInList(data);
+
+        if (!result.success) {
+            return res.status(409).json({ message: result.message });
+        }
+
+        res.status(201).json({
+            MaBN: result.data.MaBN,
+            HoTen: result.data.HoTen,
+            CCCD: result.data.CCCD,
+            GioiTinh: result.data.GioiTinh,
+            DiaChi: result.data.DiaChi,
+            MaPKB: result.data.MaPKB
+        });
+    }
+    catch (error) {
+        console.error('Error addInList: ', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+exports.removeFromList = async (req, res) => {
+    try {
+        const { NgayKham, MaBN } = req.body;
+
+        const data = { NgayKham, MaBN };
+
+        const result = await ListExamService.removeFromList(data);
+
+        if (!result.success) {
+            return res.status(404).json({ message: result.message });
+        }
+
+        res.status(200).json({
+            NgayKham,
+            MaBN
+        });
+    }
+    catch (error) {
+        console.error('Error removeFromList: ', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
+exports.getDailyList = async (req, res) => {
+    try {
+        const { NgayKham } = req.query;
+
+        const result = await ListExamService.getDailyList(NgayKham);
+
+        if (result === null) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        res.status(200).json({
+            NgayKham: result.NgayKham,
+            TongSoBenhNhan: result.TongSoBenhNhan,
+            DanhSachBenhNhan: result.DanhSachBenhNhan
+        });
+    }
+    catch (error) {
+        console.error('Error getDailyList: ', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
